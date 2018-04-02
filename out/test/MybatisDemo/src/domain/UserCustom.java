bfe702a454b26762027ff7583a3baf1a82d2481f
lirong <lirong_bupt@qq.com>
2018-04-02 19:41:06 +0800

    parameterType and resultType

--- out/test/MybatisDemo/src/domain/UserCustom.java
+++ out/test/MybatisDemo/src/domain/UserCustom.java
@@ -1,4 +1,4 @@
 package domain;
 
-public class UserCustom {
+public class UserCustom extends User {
 }
--- src/mapper/UserMapper.java
+++ src/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import domain.User;
+import domain.UserQueryVo;
 
 import java.util.List;
 
@@ -11,4 +12,8 @@ public interface UserMapper {
     public User findUserById(int id) throws Exception;
 
     public List<User> findUserByName(String name) throws Exception;
+
+    public List<User> findUserList(UserQueryVo userQueryVo) throws Exception;
+
+    public int findUserCount(UserQueryVo userQueryVo) throws Exception;
 }
